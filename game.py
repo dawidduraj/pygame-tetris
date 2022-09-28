92b4cff937e6a255fa5544898fc7f0aefb1b0e40
Dawid Duraj <dawid.duraj@deepstrm.com>
2022-09-28 12:35:26 +0200

    Setup game loop

--- game.py
+++ game.py
@@ -3,8 +3,22 @@ import pygame
 # variables/constants
 WIDTH = 300
 HEIGHT = 600
+FPS = 20
+
+gameover = False
 
 # setup
 pygame.init()
 pygame.display.set_caption("Tetris 🕹️")
 window = pygame.display.set_mode((WIDTH,HEIGHT))
+clock = pygame.time.Clock()
+
+# game loop
+while not gameover:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+    
+    clock.tick(FPS)
+
+pygame.quit()
