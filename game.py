d8432be1e2b81f833d708728c0b9918b191d976c
Dawid Duraj <dawid.duraj@deepstrm.com>
2022-09-28 12:54:40 +0200

    Create Tetris class

--- game.py
+++ game.py
@@ -1,10 +1,34 @@
 import pygame
 
+# Tetris class
+class Tetris:
+    rows = 0
+    columns = 0
+    field = []
+    score = 0
+    state = "start"
+    
+    def __init__(self, _rows, _columns):
+        self.rows = _rows
+        self.columns = _columns
+        self.field = []
+        self.score = 0
+        self.state = "start"
+
+        # create empty field
+        for i in range(_rows):
+            row = []
+            for j in range(_columns):
+                # empty field is defined with -1
+                row.append(-1)
+            self.field.append(row)
+
 # variables/constants
 WIDTH = 300
 HEIGHT = 600
 FPS = 20
 
+game = Tetris()
 gameover = False
 
 # setup
